8bc359a8f49fbbfb670331d3dac3424f26c15c8f
Const S <Const@example.com>
2023-02-19 15:05:13 +0500

    исправленна ошибка

--- src/main/java/com/example/dvdrent/services/GenreServiceImpl.java
+++ src/main/java/com/example/dvdrent/services/GenreServiceImpl.java
@@ -2,10 +2,12 @@ package com.example.dvdrent.services;
 
 import com.example.dvdrent.entities.Genre;
 import com.example.dvdrent.repositories.GenreRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class GenreServiceImpl implements GenreService {
     private final GenreRepository genreRepository;
 
--- src/main/java/com/example/dvdrent/shell/AppEventsCommands.java
+++ src/main/java/com/example/dvdrent/shell/AppEventsCommands.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.h2.tools.Console;
+import org.springframework.shell.standard.ShellOption;
 
 import java.sql.SQLException;
 import java.util.Scanner;
@@ -42,9 +43,9 @@ public class AppEventsCommands {
     }
 
     @ShellMethod(value = "Get Director By Id", key = {"gdid", "getdirectorbyid"})
-    public void getDirectorById() {
-        Scanner input = new Scanner(System.in);
-        long id = input.nextLong();
+    public void getDirectorById(@ShellOption(defaultValue = "1") long id) {
+//        Scanner input = new Scanner(System.in);
+//        long id = input.nextLong();
         System.out.println(directorService.getDirectorById(id).toString());
     }
 
